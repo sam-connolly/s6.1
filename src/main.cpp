2f75f9c7765d644dfa2655aae28ff0a332bb009b
sam-connolly <samwi5e@hotmail.co.uk>
2018-02-22 10:15:42 +0000

    Flashes between red, blue, and green

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,45 @@ Thread worker;
 EventQueue queue ;
 
 DigitalOut red(LED_RED,1); /* initial state 1 led is off */
-void flash_red(void) {
-    while(1){
-        red.write(1);
-        wait(0.5);
-        red.write(0);
-        wait(0.5);
-    }
+DigitalOut blue(LED_BLUE,1); /* initial state 1 led is off */
+DigitalOut green(LED_GREEN,1); /* initial state 1 led is off */
+
+void flash_red(void)
+{
+    red.write(0);
+    wait(0.5);
+    red.write(1);
 }
-int main() {
+
+void flash_blue(void)
+{
+    blue.write(0);
+    wait(0.5);
+    blue.write(1);
+}
+
+void flash_green(void)
+{
+    green.write(0);
+    wait(0.5);
+    green.write(1);
+}
+
+int main()
+{
 
     // put your setup code here, to run once:
     worker.start( flash_red );
 
-    while(1) {
-        // put your main code here, to run repeatedly:
+    while(1)
+    {
+        flash_red();
+        wait(0.5);
+
+        flash_blue();
+        wait(0.5);
+
+        flash_green();
+        wait(0.5);
     }
 }
